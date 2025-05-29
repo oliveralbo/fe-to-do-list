d96e923fa53262db292af4ae6e2b64f5d7edbb7f
oliveralbo <oliveralbo@hotmail.com>
2025-05-29 14:55:51 -0300

    feat: building front app

--- src/__tests__/Home.test.tsx
+++ src/__tests__/Home.test.tsx
@@ -1,11 +1,24 @@
 import { render } from '@testing-library/react';
 import { MockedProvider } from '@apollo/client/testing';
 import { Home } from '../components/Home';
+import { GetTasksDocument } from '../gql/graphql';
+
+const mocks = [
+  {
+    request: {
+      query: GetTasksDocument,
+      variables: {},
+    },
+    result: {
+      data: { tasks: [] }, // Devuelve una lista vacía
+    },
+  },
+];
 
 describe('Home Component', () => {
   it('renders without crashing', () => {
     render(
-      <MockedProvider>
+      <MockedProvider mocks={mocks}>
         <Home />
       </MockedProvider>
     );
